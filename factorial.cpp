2271ebcd86c53a0dc5bc436c1d87bcdc9cea872e
NISHITMALVIYA826 <110930029+NISHITMALVIYA826@users.noreply.github.com>
2022-10-26 12:52:49 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -15,7 +15,9 @@ unsigned int factorial(unsigned int n)
 // Driver code
 int main()
 {
-    int num = 5;
+    int num ;
+    cout<<"Enter a number = ";
+    cin>>num;
     cout << "Factorial of "
          << num << " is " << factorial(num) << endl;
     return 0;
